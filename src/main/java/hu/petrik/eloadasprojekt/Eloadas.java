602d0373379b9db9af295edf1c49ba5bce79239d
JungKata <97729333+JungKata@users.noreply.github.com>
2023-01-12 08:48:07 +0100

    asd

--- src/main/java/hu/petrik/eloadasprojekt/Eloadas.java
+++ src/main/java/hu/petrik/eloadasprojekt/Eloadas.java
@@ -1,2 +1,27 @@
-package hu.petrik.eloadasprojekt;public class Eloadas {
+package hu.petrik.eloadasprojekt;
+
+import java.io.IOException;
+
+public class Eloadas {
+    public Eloadas(int sorokSzama, int helyekSzama){
+        throw  new UnsupportedOperationException();
+    }
+
+    public boolean lefoglal(){
+        throw  new UnsupportedOperationException();
+    }
+
+    public int getSzabadHelyek(){
+        throw  new UnsupportedOperationException();
+    }
+
+    public boolean getTeli(){
+        throw  new UnsupportedOperationException();
+    }
+
+    public boolean foglalt(int sorSzam, int helySzam){
+        throw  new UnsupportedOperationException();
+    }
+
+
 }
--- src/test/java/hu/petrik/eloadasprojekt/EloadasTest.java
+++ src/test/java/hu/petrik/eloadasprojekt/EloadasTest.java
@@ -1,4 +1,29 @@
+package hu.petrik.eloadasprojekt;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EloadasTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void lefoglal() {
+    }
+
+    @Test
+    void getSzabadHelyek() {
+    }
+
+    @Test
+    void getTeli() {
+    }
+
+    @Test
+    void foglalt() {
+    }
 }
